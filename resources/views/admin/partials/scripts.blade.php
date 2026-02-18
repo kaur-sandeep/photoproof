d9d076cd663c250d18bdf5d08583c85a94bbf49b
kamal <kdev@cogniter.com>
2026-02-18 15:35:00 +0530

    scripts

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -1,5 +1,6 @@
 <script src="{{ asset('admin/js/jquery-3.7.1.min.js') }}"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
 <!-- Bootstrap 5 -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE v4 -->
