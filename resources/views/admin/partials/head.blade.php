aa706a7b8f84786613e835f8eec3da5541700f3f
Sam <samz.clark01@gmail.com>
2026-02-18 15:23:46 +0530

    added app variable

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -2,7 +2,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
  <meta name="csrf-token" content="{{ csrf_token() }}">
 <title>@yield('title', 'Admin Panel')</title>
-
+<script>
+ window.APP_URL = "{{ url('/') }}";
+ </script>
 <!-- Bootstrap 5 -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
