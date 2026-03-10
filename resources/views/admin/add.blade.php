2119a47f275cd19fa1ad4e1e62a2c78584dcecf2
Sam <samz.clark01@gmail.com>
2026-03-10 12:38:52 +0530

    Bug Fixing

--- resources/views/admin/add.blade.php
+++ resources/views/admin/add.blade.php
@@ -6,6 +6,13 @@
 
 <div class="container-fluid">
 
+
+  <div class="admin-page-header">
+    <h3 class="card-title"><b>Add Admin User </b></h3>
+    </div>
+
+
+
     <div class="row">
   @if(session('success'))
     <div class="alert alert-success">
@@ -23,31 +30,31 @@
             </div>
         @endif
         <div class="card card-primary">
-              <div class="card-header">
+              <!-- <div class="card-header">
                 <h3 class="card-title">Add Admin User</h3>
-              </div>
+              </div> -->
               <!-- /.card-header -->
               <!-- form start -->
                  <form method="POST" action="{{ route('admin.store.users') }}" enctype="multipart/form-data">
                     @csrf
-                <div class="card-body">
-                  <div class="form-group">
+                <div class="card-body row">
+                  <div class="form-group col-md-4">
                     <label for="name">Name <span class="text-danger">*</span></label>
                     <input type="name" name ="name" class="form-control" id="name" placeholder="Enter Name">
                   </div>
-                  <div class="form-group">
+                  <div class="form-group col-md-4">
                     <label for="email">Email <span class="text-danger">*</span></label>
                     <input type="email" name ="email" class="form-control" id="email" placeholder="Enter Email">
                   </div>
-                  <div class="form-group">
+                  <div class="form-group col-md-4">
                     <label for="password">Password <span class="text-danger">*</span></label>
                     <input type="password" class="form-control" name="password" id="password" placeholder="Enter Password">
                   </div>
-                   <div class="form-group">
+                   <div class="form-group col-md-4">
                     <label for="number">Phone Number <span class="text-danger">*</span></label>
                     <input type="text" name ="phone_number" class="form-control" id="number" placeholder="Enter Phone Number">
                   </div>
-                  <div class="form-group">
+                  <div class="form-group col-md-4">
                     <label for="exampleInputFile">File input</label>
                     <div class="input-group">
                       <div class="custom-file">
